381a0ffe53d557d67f424bfe6972e4542fd2f30c
JungWonYoo <88152066+JUNGWONYOO@users.noreply.github.com>
2021-12-14 00:47:24 +0900

    djust Django course 8 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def hello_world(requset):
-    return HttpResponse('하이요!') # 보여지는 내용 쓰기
+def hello_world(request):
+    return render(request, 'base.html')
 
