24832b9948048bd08b1b4deef07eb0fc90caa172
JungWonYoo <88152066+JUNGWONYOO@users.noreply.github.com>
2021-12-22 18:55:55 +0900

    django course 35 commit

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True) # SET_NULL 회원 탈퇴해도 글은 남아있음
+    
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+
+    created_at = models.DateField(auto_now_add=True, null=True) # 날짜 자동 저장
